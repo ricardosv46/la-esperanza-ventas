692332ca8992d5eb7798d8833431564a57f693cc
ricardosv46 <ricardosv46@gmail.com>
2022-09-07 13:47:07 -0500

    feat: se soluciona pequeños detalles

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,4 @@ import App from './App'
 
 import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
